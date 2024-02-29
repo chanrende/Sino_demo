84b5667efa21b5fdcf2426f70302def9bd9f4c19
Frederic <schannefrederic@outlook.de>
2024-02-29 17:33:48 +0100

    first commit

---
+++ sino_demo/src/main/java/demo/sino_demo/SinoDemoApplication.java
@@ -0,0 +1,13 @@
+package demo.sino_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinoDemoApplication.class, args);
+	}
+
+}
---
+++ sino_demo/src/test/java/demo/sino_demo/SinoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.sino_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
